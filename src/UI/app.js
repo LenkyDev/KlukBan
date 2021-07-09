507def5721749f97169276b56e944769fd590088
LinkossLink <linkosslink@gmail.com>
2021-07-09 23:22:39 +0200

    Mobile Support, Fix btn animation

--- src/UI/app.js
+++ src/UI/app.js
@@ -1,4 +1,17 @@
-const btn = document.querySelector(".card button");
+window.addEventListener("load", () => {
+    document.querySelector(".app-loader").classList.add("remove");
+    document.querySelector("main").classList.add("loaded");
+});
+
+let btn = document.querySelector(".card button");
+
+if (window.matchMedia("screen and (max-width: 1000px) and (max-height: 915px)").matches) {
+    btn = document.querySelector(".card-mobile button");
+    btn.addEventListener("click", rippleEffect, false);
+    document.querySelector(".card").remove();
+} else {
+    document.querySelector(".card-mobile").remove();
+}
 btn.addEventListener("click", click, false);
 
 function claim() {
@@ -18,4 +31,23 @@ async function click() {
     btn.classList.add("disabled");
     claim();
     btn.removeEventListener("click", click, false);
+    rippleEffect()
+};
+function rippleEffect(event) {
+    const button = event.currentTarget;
+
+    const circle = document.createElement("span");
+    const diameter = Math.max(button.clientWidth, button.clientHeight);
+    const radius = diameter / 2;
+
+    circle.style.width = circle.style.height = `${diameter}px`;
+    circle.style.left = `${event.pageX - (button.offsetLeft + radius)}px`
+    circle.style.top = `${event.pageY - (button.offsetTop + radius)}px`
+    circle.classList.add("ripple");
+
+    button.appendChild(circle);
+
+    circle.addEventListener("animationend", () => {
+        circle.remove();
+    });
 };
